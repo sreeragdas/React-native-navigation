7f05ac84f349e80f75a59541e01d39eb737aa8ff
EmbarckleSreerag <sreerag.em@embarckle.com>
2023-01-05 16:01:02 +0530

    feat(client):stack navigation added

--- App.js
+++ App.js
@@ -5,85 +5,29 @@
  * @format
  * @flow strict-local
  */
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Contact from './src/screens/contact-page/Contact';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+
+  const Stack = createNativeStackNavigator();
 
   return (
-    <View >
-      <Contact />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Contact">
+        <Stack.Screen name="Login" component={Contact} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- src/screens/contact-page/Contact.js
+++ src/screens/contact-page/Contact.js
@@ -1,50 +1,87 @@
-import react from "react";
+import react from 'react';
 import {
-    View,
-    Text,
-    StyleSheet
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
-const Contact=()=>{
-    return(
-        <View style={styles.sectionContainer} >
-            <Text style={styles.sectionTitle}>Login Page</Text>
-          
-        </View>
-    );
-}
+import React, {useState} from 'react';
+
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const click = () => {
+    if (!password || !name) {
+      alert('error');
+    } else {
+      alert('login successfull');
+    }
+  };
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Login Page</Text>
+      <TextInput
+        style={styles.input}
+        name="email"
+        onc
+        onChangeText={e => {
+          setName(e);
+        }}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        name="password"
+        onChangeText={e => {
+          setPassword(e);
+        }}
+        placeholder="Password"
+      />
+      <View style={styles.buttonView}>
+        <TouchableOpacity style={styles.button} onPress={click}>
+          <Text>Log In</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-      marginTop: 32,
-      paddingHorizontal: 24,
-    },
-    sectionTitle: {
-      fontSize: 24,
-      fontWeight: '600',
-    },
-    sectionDescription: {
-      marginTop: 8,
-      fontSize: 18,
-      fontWeight: '400',
-    },
-    highlight: {
-      fontWeight: '700',
-    },
-  });
+  sectionContainer: {
+    marginTop: 32,
+    color: '#fff',
+
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  input: {
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  button: {
+    borderWidth: 1,
+    color: 'red',
+    alignItems: 'center',
+  },
+  buttonView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Contact;
-
-// const style=StyleSheet.create({
-//     mainContainer:{
-//         height:'100%',
-//         paddingHorizontal:'30',
-//         paddingTop:'30',
-//         backgroundColor:'#fff',
-//         },
-//     mainHeader:{
-//         fontSize:25,
-//         color:'#344055',
-//         fontWeight:"500",
-//         paddingTop:20,
-//         }
-// })
