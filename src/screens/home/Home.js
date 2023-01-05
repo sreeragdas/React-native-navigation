21d7d890681ed63f296b49b3675bc82348c5fa52
EmbarckleSreerag <sreerag.em@embarckle.com>
2023-01-05 17:27:38 +0530

    chore(client):goback button added

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,12 +1,17 @@
-import {View, Text ,Button , StyleSheet} from 'react-native';
-import { create } from 'react-test-renderer';
-const Home = ({route , navigation}) => {
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {create} from 'react-test-renderer';
+const Home = ({route, navigation}) => {
   const {myName} = route.params;
   //if we are using same name variable to store then we dont want to write route.params.myName
   return (
     <View>
       <Text>Welcome {myName}</Text>
-      <Button title='GO Back' onPress={()=>{navigation.goBack()}}/>
+      <Button
+        title="GO Back"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
       {/* if we write onPress={navigation.goBack()} then in a fraction of second it will go to home and again display login page to execute the particular code we use callback funtion , it will execute the code and will work fine */}
       {}
     </View>
@@ -14,9 +19,9 @@ const Home = ({route , navigation}) => {
 };
 
 const styles = StyleSheet.create({
-    nameText:{
-        color:'black',
-    }
-})
+  nameText: {
+    color: 'black',
+  },
+});
 
 export default Home;
